dc5ca55d5de467bdb5d861644c2cefa1c9a3cd88
cuishifeng <cuishifeng0207@163.com>
2021-06-04 22:53:57 +0800

    # 初始化

---
+++ src/main/java/com/cuishifeng/efficienttool/EfficientToolApplication.java
@@ -0,0 +1,16 @@
+package com.cuishifeng.efficienttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cuishifeng
+ */
+@SpringBootApplication
+public class EfficientToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfficientToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuishifeng/efficienttool/EfficientToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuishifeng.efficienttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficientToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
